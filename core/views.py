ebf0d10e9d975f94e7c4dce911ff27b400936d53
Sebastian2710 <56756187+Sebastian2710@users.noreply.github.com>
2026-01-06 13:00:39 +0200

    Fix: keep Django project inside auction_service only

--- core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Message
-
-def hello(request):
-    message = Message.objects.first()
-    return render(request, 'core/hello.html', {'message': message})
